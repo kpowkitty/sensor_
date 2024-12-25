a001a747f75cf41d198923b7a16e9f4cd8884708
Kat <126306755+kpowkitty@users.noreply.github.com>
2024-12-25 04:16:34 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 radio.onReceivedBuffer(function (receivedBuffer) {
     message = receivedBuffer
 })
+let error = ""
 let full = false
+let logMessage = ""
 let startReceived = false
 let awaitingAcknowledgement = false
 let sendingData = false
 let message: Buffer = null
+let watchdogLimit = 10000
+let lastActionTime = input.runningTime()
+let _request = sensor.stringToBuffer("request")
 radio.setGroup(23)
 radio.setTransmitPower(7)
 let _ready = sensor.stringToBuffer("ready")
@@ -23,11 +28,14 @@ while (true) {
     awaitingAcknowledgement = true
     requesting = true
     if (!(startReceived)) {
-        basic.showString("W")
+        basic.showString("O")
         basic.pause(100)
         if (message != sensor.none() && sensor.compareBuffers(message, _start)) {
             startReceived = true
             basic.showString("S")
+            logMessage = "Start received"
+            datalogger.log(datalogger.createCV("Message", logMessage))
+            lastActionTime = input.runningTime()
         } else {
             continue;
         }
@@ -38,24 +46,48 @@ while (true) {
     if (currentTempLevel != sensor.none() && currentLightLevel != sensor.none()) {
         sensor.sendBuffer(_ready)
         basic.showString("R")
+        logMessage = "Ready sent"
+        datalogger.log(datalogger.createCV("Message", logMessage))
+        lastActionTime = input.runningTime()
     }
     while (awaitingAcknowledgement) {
+        basic.showString("W")
         basic.pause(100)
+        if (message != sensor.none() && sensor.compareBuffers(message, _request)) {
+            sensor.sendBuffer(_ready)
+        }
         if (message != sensor.none() && sensor.compareBuffers(message, _ack)) {
             basic.showString("A")
+            logMessage = "Received acknowledgement"
+            datalogger.log(datalogger.createCV("Message", logMessage))
             awaitingAcknowledgement = false
+            lastActionTime = input.runningTime()
         }
         if (message != sensor.none() && sensor.compareBuffers(message, _full)) {
             basic.showString("F")
             full = true
             awaitingAcknowledgement = false
+            lastActionTime = input.runningTime()
+        }
+        while (input.runningTime() - lastActionTime > watchdogLimit) {
+            error = "Ack Timeout"
+            datalogger.log(datalogger.createCV("Error", error))
+            basic.showString("E")
+            sensor.sendBuffer(_request)
+            basic.pause(100)
+            if (message != sensor.none() && sensor.compareBuffers(message, _ack)) {
+                lastActionTime = input.runningTime()
+            }
         }
     }
     while (sendingData) {
         if (message != sensor.none() && sensor.compareBuffers(message, _ack)) {
             sensor.sendData(currentTempLevel, currentLightLevel)
             basic.showString("D")
+            logMessage = "Successfully sent data"
+            datalogger.log(datalogger.createCV("Message", logMessage))
             sendingData = false
+            lastActionTime = input.runningTime()
         } else if (message != sensor.none() && sensor.compareBuffers(message, _full)) {
             while (full) {
                 basic.showString("F")
@@ -63,6 +95,7 @@ while (true) {
                     startReceived = false
                     full = false
                     sendingData = false
+                    lastActionTime = input.runningTime()
                 }
             }
         }
@@ -70,5 +103,6 @@ while (true) {
     currentLightLevel = sensor.none()
     currentTempLevel = sensor.none()
     message = sensor.none()
+    lastActionTime = input.runningTime()
     control.waitMicros(5000000)
 }
